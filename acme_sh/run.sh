aae50559c5685a0b4399527e61fe42d81d1482d3
Tyler Kerr <6621236+Destarianon@users.noreply.github.com>
2025-03-03 14:21:10 -0600

    Script overhaul. Added support for local ACME servers. Added support for custom key lengths and expiration times. Added some basic error checks to provided input.

--- acme_sh/run.sh
+++ acme_sh/run.sh
@@ -1,34 +1,97 @@
 #!/usr/bin/with-contenv bashio
 
-ACCOUNT=$(bashio::config 'account')
+EMAIL=$(bashio::config 'email')
 SERVER=$(bashio::config 'server')
+SERVER_ROOTCA=$(bashio::config 'server_rootca')
 DOMAINS=$(bashio::config 'domains')
 KEYFILE=$(bashio::config 'keyfile')
 CERTFILE=$(bashio::config 'certfile')
+CHALLENGE=$(bashio::config 'challenge')
+KEYLENGTH=$(bashio::config 'keylength')
+VALID_TO=$(bashio::config 'valid_to')
 DNS_PROVIDER=$(bashio::config 'dns.provider')
 DNS_ENVS=$(bashio::config 'dns.env')
+DEBUG=$(bashio::config 'debug')
 
-for env in $DNS_ENVS; do
-    export $env
-done
+ACME_ARGUMENTS=()
+ACME_SERVER_ARGUMENTS=()
+DOMAIN_ARGUMENTS=()
+ACME_CERTIFICATE_ARGUMENTS=()
 
-DOMAIN_ARR=()
-for domain in $DOMAINS; do
-    DOMAIN_ARR+=(--domain "$domain")
-done
-
-SERVER_ARG="zerossl"
-if [ -n "$SERVER" ]; then
-    SERVER_ARG="--server $SERVER"
+# Set debug argument
+if [ "${DEBUG}" == "true" ]; then
+    ACME_ARGUMENTS+=(--debug)
 fi
 
-/root/.acme.sh/acme.sh --register-account -m ${ACCOUNT} $SERVER_ARG
 
-/root/.acme.sh/acme.sh --issue "${DOMAIN_ARR[@]}" \
---dns "$DNS_PROVIDER" \
-$SERVER_ARG
+# Set challenge type
+if [ "${CHALLENGE}" == "dns" ]; then
+    bashio::log.info "Using DNS challenge"
+    ACME_ARGUMENTS+=(--dns "$DNS_PROVIDER")
+    for env in $DNS_ENVS; do
+        export $env
+    done
 
-/root/.acme.sh/acme.sh --install-cert "${DOMAIN_ARR[@]}" \
+elif [ "${CHALLENGE}" == "http" ]; then
+    bashio::log.info "Using HTTP challenge"
+
+elif [ "${CHALLENGE}" == "tls" ]; then
+    bashio::log.info "Using TLS ALPN challenge"
+    ACME_ARGUMENTS+=(--alpn)
+
+else
+    bashio::log.error "Invalid challenge type"
+    exit 1
+fi
+
+
+# Set domain arguments
+if [ ${#DOMAINS[@]} -eq 0 ]; then
+    bashio::log.error "No domains were configured"
+    exit 1
+else
+    for domain in $DOMAINS; do
+        DOMAIN_ARGUMENTS+=(-d "$domain")
+    done
+fi
+
+
+# Set server arguments
+if [ -n "$SERVER" ]; then
+    bashio::log.info "Using custom acme server: '$SERVER'"
+    ACME_SERVER_ARGUMENTS="--server $SERVER"
+    if [ -n "$SERVER_ROOTCA" ]; then
+        bashio::log.info "Using custom root CA: \n$SERVER_ROOTCA"
+        echo "${SERVER_ROOTCA}" > /tmp/root-ca-cert.crt
+        ACME_SERVER_ARGUMENTS+=(--ca-bundle "/tmp/root-ca-cert.crt")
+    fi
+else
+    bashio::log.info "Using default letsencrypt server"
+    ACME_SERVER_ARGUMENTS="--server letsencrypt"
+fi
+
+
+# Set custom keylength argument
+if [ -n "$KEYLENGTH" ]; then
+    bashio::log.info "Requesting certificate key length of: '$KEYLENGTH'"
+    ACME_CERTIFICATE_ARGUMENTS+=(--keylength $KEYLENGTH)
+fi
+
+
+# Set custom expiration date argument
+if [ -n "$VALID_TO" ]; then
+    bashio::log.info "Requesting certificate expiration date of: '$VALID_TO'"
+    ACME_CERTIFICATE_ARGUMENTS+=(--valid-to "$VALID_TO")
+fi
+
+
+/root/.acme.sh/acme.sh --issue --force \
+"${ACME_ARGUMENTS[@]}" \
+"${DOMAIN_ARGUMENTS[@]}" \
+"${ACME_SERVER_ARGUMENTS[@]}" \
+"${ACME_CERTIFICATE_ARGUMENTS[@]}"
+
+/root/.acme.sh/acme.sh --install-cert "${DOMAIN_ARGUMENTS[@]}" \
 --fullchain-file "/ssl/${CERTFILE}" \
 --key-file "/ssl/${KEYFILE}" \
 
